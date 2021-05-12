db4dea48186e80087c807ae909d38bf8175801b6
abrah <abr864@gmail.com>
2021-05-12 18:59:11 -0500

    volví a modificar el nombre --abraham

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'abuelito dime tu 1234',
+      title: 'abuelito dime tu',
       theme: ThemeData(
         // This is the theme of your application.
         //

96c94f025cab0025f060336dc78d72b85b3ead5b
abrah <abr864@gmail.com>
2021-05-12 19:42:11 -0500

    cambie el nombre a test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'abuelito dime tu tst',
+      title: 'abuelito dime tu test',
       theme: ThemeData(
         // This is the theme of your application.
         //
